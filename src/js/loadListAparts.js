c97a93ab7db346883545160f0997e31a22333d7b
Roman Kuzmich <87143625+psih2131@users.noreply.github.com>
2025-11-28 19:53:59 +0300

    fix

---
+++ src/js/loadListAparts.js
@@ -0,0 +1,18 @@
+export default function loadApart() {
+    // async function serverRequest() {
+    //     try {
+    //         const response = await fetch('https://ecatalog-service.nmarket.pro/BasePro/?login=ssafonenko691_yandex_ru&password=4ZW5V9bdE2&regionGroupId=77')
+
+    //         if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`)
+
+    //         const xmlText = await response.text() // получаем XML как текст
+    //         console.log(xmlText)
+    //     } catch (error) {
+    //         console.error('Ошибка при запросе:', error)
+    //     }
+    // }
+
+    // serverRequest()
+
+
+}
--- src/js/main.js
+++ src/js/main.js
@@ -21,6 +21,8 @@ import readMoreReview from './objectReviewReadMore.js'
 
 import readMoreHome from './readMoreHomeBtn.js'
 
+import loadApart from './loadListAparts.js'
+
 
 removeElementorStyle()
 
@@ -49,6 +51,28 @@ simmilarSlider()
 simmilarSliderResizeBoxHeight()
 
 readMoreHome()
+
+loadApart()
+
+
+
+async function serverRequest() {
+    try {
+        const response = await fetch('https://ecatalog-service.nmarket.pro/BasePro/?login=ssafonenko691_yandex_ru&password=4ZW5V9bdE2&regionGroupId=77')
+
+        if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`)
+
+        const xmlText = await response.text() // получаем XML как текст
+        console.log(xmlText)
+    } catch (error) {
+        console.error('Ошибка при запросе:', error)
+    }
+}
+
+serverRequest()
+
+
+
 // switchThemeColor()
 
 
